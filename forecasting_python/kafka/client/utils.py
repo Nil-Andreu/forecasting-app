f8c4f29b4a05c95e47a97a1fec947c16e70a2894
Nil-Andreu <nilandreug@gmail.com>
2022-09-24 17:33:58 +0200

    [NEW] Error function in util file

---
+++ forecasting_python/kafka/client/utils.py
@@ -0,0 +1,17 @@
+from confluent_kafka import KafkaError, KafkaException
+
+def error_cb(err):
+    """ The error callback is used for generic client errors. These
+        errors are generally to be considered informational as the client will
+        automatically try to recover from all errors, and no extra action
+        is typically required by the application.
+        For this example however, we terminate the application if the client
+        is unable to connect to any broker (_ALL_BROKERS_DOWN) and on
+        authentication errors (_AUTHENTICATION). """
+
+    print("Client error: {}".format(err))
+    if err.code() == KafkaError._ALL_BROKERS_DOWN or \
+       err.code() == KafkaError._AUTHENTICATION:
+        # Any exception raised from this callback will be re-raised from the
+        # triggering flush() or poll() call.
+        raise KafkaException(err)

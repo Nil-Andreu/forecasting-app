9e63dcec4d025b0003b92162f4f2768ffcd6a2ee
Nil-Andreu <nilandreug@gmail.com>
2022-09-25 10:29:28 +0200

    [NEW] Renaming folder

--- backend_golang/kafka/kafka_consumer.go
+++ backend_golang/kafka/kafka_consumer.go
@@ -6,9 +6,11 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/joho/godotenv"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func kafka_consumer() {
-
+	want := "Hello world"
+	fmt.Println(want)
 }
--- backend_golang/main.go
+++ backend_golang/main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	want := "Hello world"
-	fmt
+	fmt.Println(want)
 }

af9c0bbd23f7979f8055931426fa6611bff3c7a4
Nil-Andreu <nilandreug@gmail.com>
2022-09-25 11:15:53 +0200

    [NEW] Defining KafKa Consumer

--- backend_golang/kafka/kafka_consumer.go
+++ backend_golang/kafka/kafka_consumer.go
@@ -1,4 +1,4 @@
-package kafka
+package main
 
 import (
 	"fmt"
@@ -11,20 +11,20 @@ import (
 	_ "github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-func kafka_consumer() {
+func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	KAFKA_API_KEY := os.Getenv("KAFKA_API_KEY")
-	/* KAFKA_SECRET_API_KEY := os.Getenv("KAFKA_SECRET_API_KEY")
+	KAFKA_SECRET_API_KEY := os.Getenv("KAFKA_SECRET_API_KEY")
 	KAFKA_GROUP_ID := os.Getenv("KAFKA_GROUP_ID")
 	KAFKA_CONSUMER_ID := os.Getenv("KAFKA_CONSUMER_ID")
 	KAFKA_PRODUCER_ID := os.Getenv("KAFKA_PRODUCER_ID")
 	BOOTSTRAP_SERVER := os.Getenv("BOOTSTRAP_SERVER")
 	SECURITY_PROTOCOL := os.Getenv("SECURITY_PROTOCOL")
-	SASL_MECHANISM := os.Getenv("SASL_MECHANISM")*/
+	SASL_MECHANISM := os.Getenv("SASL_MECHANISM")
 
 	fmt.Println(KAFKA_API_KEY)
 }
--- backend_golang/main.go
+++ backend_golang/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+)
 
 func main() {
 	want := "Hello world"

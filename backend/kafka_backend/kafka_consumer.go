36b7b7368400ad40a227550ade752ab757df2395
Nil-Andreu <nilandreug@gmail.com>
2022-09-25 14:04:18 +0200

    [NEW] Running kafka consumer

--- backend/kafka_backend/kafka_consumer.go
+++ backend/kafka_backend/kafka_consumer.go
@@ -56,12 +56,61 @@ func create_consumer(
 }
 
 
-func Kafka_consumer() (*kafka.Consumer) {
+func Kafka_consumer() {
 	// We obtain first the environmental variables that we want
 	KAFKA_API_KEY, KAFKA_SECRET_API_KEY, KAFKA_GROUP_ID, BOOTSTRAP_SERVER, SECURITY_PROTOCOL, SASL_MECHANISM := read_env_consumer()
 	
 	consumer := create_consumer(KAFKA_API_KEY, KAFKA_SECRET_API_KEY, KAFKA_GROUP_ID, 
 								BOOTSTRAP_SERVER, SECURITY_PROTOCOL, SASL_MECHANISM)
 
-	return consumer
+
+	// And now we will consume the services
+	err := consumer.SubscribeTopics([]string{"financials"}, nil)
+
+	if err != nil {
+		log.Fatal("Failed to suscribe to Topic")
+	}
+
+	// And now we run the consumer
+	run := true
+
+	for run {
+		// Poll the new messages
+		event := consumer.Poll(1)
+
+		// No new events
+		if event == nil {
+			continue
+		}
+
+		// And now depending on the event type, we would do a different thing
+		switch e := event.(type) {
+			case *kafka.Message:
+				fmt.Printf("%% Message on %s:\n%s\n", e.TopicPartition, string(e.Value))
+
+				if e.Headers != nil {
+					fmt.Printf("%% Headers: %v\n", e.Headers)
+				}
+
+				_, err := consumer.StoreMessage(e)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "%% Error storing offset after message %s:\n",
+						e.TopicPartition)
+				}
+
+			case kafka.Error:
+				// Print information about this error
+				fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
+
+				// If all brokers are down, we stop consuming
+				if e.Code() == kafka.ErrAllBrokersDown {
+					run = false
+				}
+			default:
+				fmt.Printf("Ignored %v\n", e)
+		}
+	}
+
+	fmt.Printf("Closing consumer")
+	consumer.Close()
 }

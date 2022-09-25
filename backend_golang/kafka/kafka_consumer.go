09c66a089c0154b730152cd95e9ab1fbfe50a64e
Nil-Andreu <nilandreug@gmail.com>
2022-09-25 10:44:22 +0200

    [NEW] KafKa packages defined

--- backend_golang/kafka/kafka_consumer.go
+++ backend_golang/kafka/kafka_consumer.go
@@ -3,14 +3,28 @@ package kafka
 import (
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
+	"log"
+	_ "os/signal"
+	_ "syscall"
 
 	"github.com/joho/godotenv"
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	_ "github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func kafka_consumer() {
-	want := "Hello world"
-	fmt.Println(want)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	KAFKA_API_KEY := os.Getenv("KAFKA_API_KEY")
+	/* KAFKA_SECRET_API_KEY := os.Getenv("KAFKA_SECRET_API_KEY")
+	KAFKA_GROUP_ID := os.Getenv("KAFKA_GROUP_ID")
+	KAFKA_CONSUMER_ID := os.Getenv("KAFKA_CONSUMER_ID")
+	KAFKA_PRODUCER_ID := os.Getenv("KAFKA_PRODUCER_ID")
+	BOOTSTRAP_SERVER := os.Getenv("BOOTSTRAP_SERVER")
+	SECURITY_PROTOCOL := os.Getenv("SECURITY_PROTOCOL")
+	SASL_MECHANISM := os.Getenv("SASL_MECHANISM")*/
+
+	fmt.Println(KAFKA_API_KEY)
 }
---
+++ backend_golang/kafka/kafka_producer.go
@@ -0,0 +1,8 @@
+package kafka
+
+import "fmt"
+
+func kafka_producer() {
+	want := "Hello world"
+	fmt.Println(want)
+}

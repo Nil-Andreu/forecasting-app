99458e62c9777c3a4f99138428a3d7026ace4cfb
Nil-Andreu <nilandreug@gmail.com>
2022-09-25 13:22:10 +0200

    [NEW] Defining package as not mod

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	_ "fmt"
 
-	"./kafka_backend"
+	"./kafka_backend"  // because we have modules off
 )
 
 func main() {
 	kafka_backend.Kafka_consumer()
-	// fmt.Println(want)
+
+	// fmt.Println(consumer)
 }

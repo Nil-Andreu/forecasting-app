d654f0f8fa291b77af502d63b9408c1dfb9ab426
Nil-Andreu <nilandreug@gmail.com>
2022-09-26 19:24:05 +0200

    [NEW] Docker entrypoint sh

---
+++ injestion/notebooks/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script to install Linux System Tools and Basic Python Components
+apt-get update   # updates the package index cache
+apt-get upgrade -y    # updates packages
+
+# Installation of system tools
+apt-get install -y bzip2 gcc git
+apt-get install -y htop screen vim wget
+apt-get upgrade -y bash
+apt-get clean   # cleans up package index cache
+
+# Installation of miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda.sh   # download of miniconda
+bash Miniconda.sh  # installation of miniconda
+export PATH="/root/miniconda3/bin:$PATH"  # pre-pends the path
+
+# Installation of python libraries
+conda install -y pandas
+conda install -y ipython

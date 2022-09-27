9b5c686f4b014b9815267848ea99b961f65ae23b
Nil-Andreu <nilandreug@gmail.com>
2022-09-27 09:51:46 +0200

    [NEW] Rename of the package kafka

--- backend/kafka/kafka_producer.go
+++ backend/kafka/kafka_producer.go
@@ -1,4 +1,4 @@
-package kafka_backend
+package kafka
 
 import (
 	"fmt"
--- backend/main.go
+++ backend/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	_ "fmt"
 
-	"./kafka_backend"  // because we have modules off
+	"./kafka"  // because we have modules off
 )
 
 func main() {
-	kafka_backend.Kafka_consumer()
-	kafka_backend.Kafka_producer()
+	kafka.Kafka_consumer()
+	kafka.Kafka_producer()
 
 	// fmt.Println(consumer)
 }
